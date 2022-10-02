3b55cf738d0478d229b273248b3f2cf01b4eed8c
bukiz-cloud <adeniyibukunmiadetunji@gmail.com>
2022-10-02 22:09:16 +0100

    adding the Hello world to the file

---
+++ src/com/dufuna/berlin/bukunmiadeniyi/address/AddressApp.java
@@ -0,0 +1,10 @@
+package com.dufuna.berlin.bukunmiadeniyi.address;
+
+public class AddressApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+
+
+}
